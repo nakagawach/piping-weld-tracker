96411606b9a0e35584426c443f3041fffbdbaf2b
nakagawach <85813915+nakagawach@users.noreply.github.com>
2026-08-22 20:28:25 +0900

    Add sample PDF URL to Flask view

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+SAMPLE_PDF_URL = (
+    "https://img1.wsimg.com/blobby/go/3daad7b2-98c5-4dc1-b37a-5570afcba267/"
+    "downloads/pipeline_isometric_drawing.pdf"
+)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", sample_pdf_url=SAMPLE_PDF_URL)
 
 
 if __name__ == "__main__":
